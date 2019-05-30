5aab19e758264162040c9675c5691971e28aba25
artemchepeliuk <4epeliuk@gmail.com>
2019-05-30 18:10:28 +0300

    added footer section

--- js/common.js
+++ js/common.js
@@ -1,5 +1,33 @@
-$(function() {
+$(function () {
 
-	
+  $('.open_menu').on('click', function () {
+    $('.hide_menu').toggleClass('hide_menu_active');
+    $('body').toggleClass('overflow');
+  });
+
+
+  $(document).ready(function () {
+    $(".menu-nav").on("click", "a", function (event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),
+        top = $(id).offset().top;
+      $('.hide_menu').toggleClass('hide_menu_active');
+      $('body').toggleClass('overflow');
+      $('body,html').animate({
+        scrollTop: top
+      }, 1000);
+    });
+  });
+
+
+  var totop = $('#toTop');
+  console.log(totop)
+
+  $(totop).on('click', function () {
+    console.log(totop)
+    $('body,html').animate({
+      scrollTop: 0
+    }, 1000);
+    })
 
 });
